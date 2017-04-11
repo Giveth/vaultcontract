ac6e77b2a8e3b2f203d481683c81b5a2b92d2ff6
Adria Massanet <adria@codecontext.io>
2017-04-11 16:47:58 +0200

    Remove unused contracts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,9 +1,5 @@
-var Owned = artifacts.require("./Owned.sol");
-var Escapable = artifacts.require("./Escapable.sol");
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(Owned);
-  deployer.deploy(Escapable);
 };
