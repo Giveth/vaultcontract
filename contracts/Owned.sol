37eb6973447ee5fecf56399eaf06e55e1e49b9de
Adria Massanet <adria@codecontext.io>
2017-04-13 12:16:41 +0200

    Added contracts from develop branch

--- contracts/Owned.sol
+++ contracts/Owned.sol
@@ -1,22 +1,5 @@
 pragma solidity ^0.4.6;
 
-/*
-    Copyright 2016, Jordi Baylina
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 /// @dev `Owned` is a base level contract that assigns an `owner` that can be
 ///  later changed
 contract Owned {
@@ -31,7 +14,7 @@ contract Owned {
 
     /// @notice `owner` can step down and assign some other address to this role
     /// @param _newOwner The address of the new owner. 0x0 can be used to create
-    ///  an unowned neutral vault, however that cannot be undone
+    ///  an unowned contract, however that cannot be undone
     function changeOwner(address _newOwner) onlyOwner {
         owner = _newOwner;
         NewOwner(msg.sender, _newOwner);
