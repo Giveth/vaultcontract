eb66e808ea6e15ebfe36f03298a92b5d49e85937
Adrià Massanet <adria@codecontext.io>
2017-04-20 12:46:05 +0200

    Fixed assertJump checks

--- test/Owned.js
+++ test/Owned.js
@@ -1,40 +1,41 @@
 /* eslint no-undef: "off" */
 
-const assertJump = require("./helpers/assertJump.js");
+const assertJump = require("./helpers/assertJump.js")
 
-const Owned = artifacts.require("../contracts/Owned.sol");
+const Owned = artifacts.require("../contracts/Owned.sol")
 
-contract("Owned", (accounts) => {
-    let owned;
+contract("Owned", accounts => {
+  let owned
 
-    beforeEach(async () => {
-        owned = await Owned.new();
-    });
+  beforeEach(async () => {
+    owned = await Owned.new()
+  })
 
-    it("should have an owner", async () => {
-        const owner = await owned.owner();
-        assert.isTrue(owner !== 0);
-    });
+  it("should have an owner", async () => {
+    const owner = await owned.owner()
+    assert.isTrue(owner !== 0)
+  })
 
-    it("changes owner after transfer", async () => {
-        const other = accounts[ 1 ];
-        await owned.changeOwner(other);
-        const owner = await owned.owner();
+  it("changes owner after transfer", async () => {
+    const other = accounts[1]
+    await owned.changeOwner(other)
+    const owner = await owned.owner()
 
-        assert.isTrue(owner === other);
-    });
+    assert.isTrue(owner === other)
+  })
 
-    it("should prevent non-owners from transfering", async () => {
-        const other = accounts[ 2 ];
-        const owner = await owned.owner.call();
-        assert.isTrue(owner !== other);
-        try {
-            await owned.changeOwner(other, { from: other });
-        } catch (error) {
-            assertJump(error);
-        }
-    });
-/*
+  it("should prevent non-owners from transfering", async () => {
+    const other = accounts[2]
+    const owner = await owned.owner.call()
+    assert.isTrue(owner !== other)
+    try {
+      await owned.changeOwner(other, { from: other })
+    } catch (error) {
+      return assertJump(error)
+    }
+    assert.fail("should have thrown before")
+  })
+  /*
   it('should guard ownership against stuck state', async function() {
     let originalOwner = await owned.owner();
     await owned.changeOwner(null, {from: originalOwner});
@@ -43,4 +44,4 @@ contract("Owned", (accounts) => {
     assert.equal(originalOwner, newOwner);
   });
 */
-});
+})
